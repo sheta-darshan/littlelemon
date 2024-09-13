76ed7ce606c6868b0cc653d11ffd86420004f4ec
Student shetad <shetadarshan@yahoo.com>
2024-09-13 05:51:54 +0530

    app aupdated

--- littlelemon/Restaurant/models.py
+++ littlelemon/Restaurant/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
-# Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(validators=[MaxValueValidator(6)])
+    booking_date = models.DateField()
+    
+    def __str__(self):
+        return str(self.name)
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MaxValueValidator(99999999.99)]  # Ensures maximum value is less than or equal to 99999999.99
+    )
+    inventory = models.IntegerField(validators=[MaxValueValidator(99999)])
+    
+    def __str__(self):
+        return str(self.title)
