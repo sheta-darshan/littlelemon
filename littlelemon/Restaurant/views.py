de0eaf169f0c354adca53e5325993cdf88108254
Student shetad <shetadarshan@yahoo.com>
2024-09-14 06:42:17 +0530

    add more code

--- littlelemon/Restaurant/views.py
+++ littlelemon/Restaurant/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from .models import Menu, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
 
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemsView(generics.ListCreateAPIView):
+    serializer_class = MenuItemSerializer
+    queryset = Menu.objects.all()
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    serializer_class = MenuItemSerializer
+    queryset = Menu.objects.all()
+    
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    
+    
