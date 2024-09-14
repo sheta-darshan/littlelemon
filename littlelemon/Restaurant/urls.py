95c466371e6aa6c3549c80ec14bc0992c74fed0b
Student shetad <shetadarshan@yahoo.com>
2024-09-14 06:51:52 +0530

    add more code

--- littlelemon/Restaurant/urls.py
+++ littlelemon/Restaurant/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('menu/', views.MenuItemsView.as_view(), name='menu-items'),
     path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='single-menu-item'),
-    path('/booking', include(router.urls)),
+    path('restaurant/booking', include(router.urls)),
 ]
